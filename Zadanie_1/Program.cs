26f87286322e51d7a80609656c68ba408eead9e7
Jakub <kuba.biniek01@gmail.com>
2024-03-11 17:55:58 +0100

    Modyfikacja 2

--- Zadanie_1/Program.cs
+++ Zadanie_1/Program.cs
@@ -5,6 +5,8 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+
+            List<int> valuesList = new List<int>() { 1, 2, 3, 4, 5, 6, };
         }
     }
 }
